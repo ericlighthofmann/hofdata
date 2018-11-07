da9583f4f0fd6ccd4da5259d4862f99623de46ff
Eric Hofmann <ericlighthofmann@gmail.com>
2018-11-07 09:24:29 -0800

    front page up & running

--- home/models.py
+++ home/models.py
@@ -4,9 +4,17 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from blog.models import BlogPage, BlogIndexPage
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = BlogIndexPage.objects.get().get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
