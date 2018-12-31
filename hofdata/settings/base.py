5080a347fd7dd5fd79ba09545a0fc67f73e49b0e
Eric hofmann <ericlighthofmann@gmail.com>
2018-12-31 15:41:52 -0800

    messing with settings

--- hofdata/settings/base.py
+++ hofdata/settings/base.py
@@ -163,7 +163,7 @@ WAGTAIL_SITE_NAME = "hofdata"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://hofdata.com'
 
 #Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- hofdata/settings/production.py
+++ hofdata/settings/production.py
@@ -1,6 +1,13 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7!sc##evq!k!1kbgk5mhnj-ou03z@c6$+=tqzro#@gozd=&w$y'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['hofdata.com', 'www.hofdata.com', '68.183.169.105']
 
 try:
     from .local import *
